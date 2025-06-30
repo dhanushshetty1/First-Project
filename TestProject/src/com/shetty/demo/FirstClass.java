dd35b907dfe2b67ef4acc39baa2c341028031485
dhanushshetty1 <dhanushshetty106@gmail.com>
2025-06-30 09:45:01 +0530

    first commit

---
+++ TestProject/src/com/shetty/demo/FirstClass.java
@@ -0,0 +1,9 @@
+package com.shetty.demo;
+
+public class FirstClass {
+	public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
